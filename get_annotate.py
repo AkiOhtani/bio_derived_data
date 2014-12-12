9d98965ba94f89fb85cde26d71ef07477969f13e
Akiyoshi Ohtani <otani@a2.keio.jp>
2014-12-12 18:56:24 +0900

    fixed bug

--- get_annotate.py
+++ get_annotate.py
@@ -5,14 +5,13 @@ from Bio import SeqIO
 import logging
 import os, sys
 
+f = open("annotate_prot.txt", "w")
+
 def write_record(readFile, writeFile, annotation):
     with open(readFile, 'r') as fp:
-        with open(writeFile, 'w') as f:
-            for seq_record in SeqIO.parse(fp, "fasta"):
-                print(writeFile + ":", seq_record.id)
-                f.write(str(' '.join([amino for amino in seq_record.seq])) + "," + annotation + "\n")
-                print(str(' '.join([amino for amino in seq_record.seq])) + "," + annotation)
-        f.close
+        for seq_record in SeqIO.parse(fp, "fasta"):
+            f.write(str(' '.join([amino for amino in seq_record.seq])) + "," + annotation + "\n")
+            print(str(' '.join([amino for amino in seq_record.seq])) + "," + annotation)
     fp.close
 
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -21,12 +20,15 @@ logging.info("running %s" % " ".join(sys.argv))
 print("===============================================================")
 print("=================結晶化するタンパク質==========================")
 print("===============================================================")
-write_record("nr50_30to200_XRAY.txt", "annotate_prot.txt", "1")
+print("bin02_nr50_30to200_XRAY.txt")
+write_record("bin02_nr50_30to200_XRAY.txt", "annotate_prot.txt", "1")
 
 print("===============================================================")
 print("=================結晶化しないタンパク質==========================")
 print("===============================================================")
-write_record("nr50_30to200_NMR_ONLY.txt", "annotate_prot.txt", "0")
+print("bin02_nr50_30to200_NMR_ONLY.txt")
+write_record("bin02_nr50_30to200_NMR_ONLY.txt", "annotate_prot.txt", "0")
 
+f.close
 program = os.path.basename(sys.argv[0])
 logging.info("finished running %s" % program)
